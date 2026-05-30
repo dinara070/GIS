7c2e67b1813b88810a5d1db8fb1339c8c46850b1
Liza_Programm <125730654+dinara070@users.noreply.github.com>
2026-05-30 10:29:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ USERS_DB = {
     "admin": {
         "password": "admin2026",
         "role": "admin",
-        "display_name": "Адміністратор Петренко І.М.",
+        "display_name": "Адміністратор Чорна Є. М.",
         "subdivision": "ІТ-відділ АТ «Вінницяобленерго»"
     },
     "brigade1": {
