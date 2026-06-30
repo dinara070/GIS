a29651768d8eb694d012ce1c689e0af93e866309
Liza_Programm <125730654+dinara070@users.noreply.github.com>
2026-06-30 09:20:04 +0300

    Remove unused matplotlib imports from app.py
    Removed unused matplotlib imports.

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-from matplotlib.colors import ListedColormap
 import json
 import io
 import datetime
